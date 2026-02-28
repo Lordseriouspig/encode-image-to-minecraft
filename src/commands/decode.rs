fdecb18dba376e2a552552275d91e9101dabc24d
Lordseriouspig <lordseriouspig@gmail.com>
2026-02-28 17:07:17 +1000

    style: my linter ran again

--- src/commands/decode.rs
+++ src/commands/decode.rs
@@ -1,24 +1,27 @@
 // Copyright (C) 2026 Lordseriouspig
-// 
+//
 // This file is part of encode-image-to-minecraft.
-// 
+//
 // encode-image-to-minecraft is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
-// 
+//
 // encode-image-to-minecraft is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
-// 
+//
 // You should have received a copy of the GNU General Public License
 // along with encode-image-to-minecraft.  If not, see <https://www.gnu.org/licenses/>.
 
+use crate::helpers::read::read_region_buf;
 use anyhow::{Context, Result};
 use clap::Args;
-use std::{fs::File, io::{Read, Write}};
-use crate::helpers::read::read_region_buf;
+use std::{
+    fs::File,
+    io::{Read, Write},
+};
 
 #[derive(Args, Debug)]
 pub struct DecodeCmd {
--- src/commands/mod.rs
+++ src/commands/mod.rs
@@ -1,29 +1,29 @@
 // Copyright (C) 2026 Lordseriouspig
-// 
+//
 // This file is part of encode-image-to-minecraft.
-// 
+//
 // encode-image-to-minecraft is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
-// 
+//
 // encode-image-to-minecraft is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
-// 
+//
 // You should have received a copy of the GNU General Public License
 // along with encode-image-to-minecraft.  If not, see <https://www.gnu.org/licenses/>.
 
 use clap::{Parser, Subcommand};
-pub mod encode;
 pub mod decode;
+pub mod encode;
 
 #[derive(Parser, Debug)]
 #[clap(name = "mc-encode", version, author, about)]
 pub struct Args {
     #[clap(subcommand)]
-    pub entity_type: Command
+    pub entity_type: Command,
 }
 
 #[derive(Subcommand, Debug)]
